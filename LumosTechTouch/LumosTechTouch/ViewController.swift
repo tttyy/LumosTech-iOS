7088ef933df153278493a23e7e2ef2727642a7f0
tttyy <tttyy@Yes-MacBook-Pro.local>
2014-06-29 17:29:42 -0700

    Project created

---
+++ LumosTechTouch/LumosTechTouch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LumosTechTouch
+//
+//  Created by Ye Tian on 6/29/14.
+//  Copyright (c) 2014 LumosTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
